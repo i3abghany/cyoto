f6619dae2e6f9f9d47e15afceb6cd39ca0a860c1
Mahmoud Abumandour <ma.mandourr@gmail.com>
2024-10-04 19:30:34 -0700

    Add basic parse code

--- parse.go
+++ parse.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"Kyoto/pkg/parser"
+	"github.com/antlr4-go/antlr/v4"
+)
+
+type KyotoListener struct {
+	*parser.BaseKyotoGrammarListener
+}
+
+func (s *KyotoListener) VisitErrorNode(node antlr.ErrorNode) {
+	println("Error: ", node.GetText())
+}
+
+func (s *KyotoListener) EnterProgram(ctx *parser.ProgramContext) {
+	println("Program: ", ctx.GetText())
+}
+
+func parse(code string) {
+	inputStream := antlr.NewInputStream(code)
+	lexer := parser.NewKyotoGrammarLexer(inputStream)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	p := parser.NewKyotoGrammarParser(stream)
+	antlr.ParseTreeWalkerDefault.Walk(&KyotoListener{}, p.Program())
+}
